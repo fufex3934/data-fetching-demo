ef4e5cd6655b054f53e6d135962c8fb801db922b
fufex3934 <fufawakgari174@gmail.com>
2025-04-16 08:35:33 +0300

    form validation with useActionState

--- src/app/products-db-create/page.tsx
+++ src/app/products-db-create/page.tsx
@@ -1,48 +1,61 @@
+"use client";
 import { Submit } from "@/components/submit";
-import { addProduct } from "@/prisma-db";
-import { redirect } from "next/navigation";
 
-async function createProduct(formData: FormData) {
-    "use server";
-    const title = formData.get("title") as string;
-    const price = formData.get("price") as string;
-    const description = formData.get("description") as string;
+import { useActionState } from "react";
+import { FormState,createProduct } from "@/actions/products";
 
-   await addProduct(title,parseInt(price),description);
-   redirect("/products-db");
 
-    
-}
+
+
 export default function AddProductPage() {
+  const initialsState: FormState = {
+    errors: {},
+  };
+  const [state, formAction, isPending] = useActionState(
+    createProduct,
+    initialsState
+  );
   return (
-    <form action={createProduct} className="p-4 space-y-4 max-w-96">
-      <label className="text-white">
-        Title
-        <input
-          type="text"
-          className="block w-full p-2 text-white border rounded"
-          name="title"
-       
-        />
-      </label>
-      <label className="text-white">
-        Price
-        <input
-          type="number"
-          className="block w-full p-2 text-white border rounded"
-          name="price"
-          
-        />
-      </label>
-      <label className="text-white">
-        Description
-        <textarea
-          className="block w-full p-2 text-white border rounded"
-          name="description"
-        
-        />
-      </label>
-     <Submit/>
+    <form action={formAction} className="p-4 space-y-4 max-w-96">
+      <div>
+        <label className="text-white">
+          Title
+          <input
+            type="text"
+            className="block w-full p-2 text-white border rounded"
+            name="title"
+          />
+        </label>
+        {state.errors.title && (
+          <p className="text-red-500">{state.errors.title}</p>
+        )}
+      </div>
+      <div>
+        <label className="text-white">
+          Price
+          <input
+            type="number"
+            className="block w-full p-2 text-white border rounded"
+            name="price"
+          />
+        </label>
+        {state.errors.price && (
+          <p className="text-red-500">{state.errors.price}</p>
+        )}
+      </div>
+      <div>
+        <label className="text-white">
+          Description
+          <textarea
+            className="block w-full p-2 text-white border rounded"
+            name="description"
+          />
+        </label>
+        {state.errors.description && (
+          <p className="text-red-500">{state.errors.description}</p>
+        )}
+      </div>
+      <Submit />
     </form>
-  )
+  );
 }
