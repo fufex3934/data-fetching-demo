062a97561679b3fcd24236ed5e8e3f094473697f
fufex3934 <fufawakgari174@gmail.com>
2025-04-16 09:06:02 +0300

    update server action

---
+++ src/app/products-db/[id]/product-edit-form.tsx
@@ -0,0 +1,70 @@
+"use client";
+import { Submit } from "@/components/submit";
+
+import { useActionState } from "react";
+import { FormState ,editProduct} from "@/actions/products";
+import { getProduct } from "@/prisma-db";
+import { Product } from "../page";
+
+
+
+
+export default  function EditProductForm({product}:{product:Product}) {
+  const initialsState: FormState = {
+    errors: {},
+  };
+
+  const editProductWithId = editProduct.bind(null,product.id);
+  const [state, formAction, isPending] = useActionState(
+  editProductWithId,
+    initialsState
+  );
+ 
+  return (
+    <form action={formAction} className="p-4 space-y-4 max-w-96">
+        
+      <div>
+        <label className="text-white">
+          Title
+          <input
+            type="text"
+            className="block w-full p-2 text-white border rounded"
+            name="title"
+            defaultValue={product.title}
+          />
+        </label>
+        {state.errors.title && (
+          <p className="text-red-500">{state.errors.title}</p>
+        )}
+      </div>
+      <div>
+        <label className="text-white">
+          Price
+          <input
+            type="number"
+            className="block w-full p-2 text-white border rounded"
+            name="price"
+            defaultValue={product.price}
+          />
+        </label>
+        {state.errors.price && (
+          <p className="text-red-500">{state.errors.price}</p>
+        )}
+      </div>
+      <div>
+        <label className="text-white">
+          Description
+          <textarea
+            className="block w-full p-2 text-white border rounded"
+            name="description"
+            defaultValue={product.description ?? ""}
+          />
+        </label>
+        {state.errors.description && (
+          <p className="text-red-500">{state.errors.description}</p>
+        )}
+      </div>
+      <Submit />
+    </form>
+  );
+}
--- src/app/products-db/page.tsx
+++ src/app/products-db/page.tsx
@@ -1,5 +1,5 @@
 import { getProducts } from "@/prisma-db";
-
+import Link from "next/link";
 export type Product = {
   id: number;
   title: string;
@@ -17,7 +17,10 @@ export default async function ProductsDBPage() {
           key={product.id}
           className="p-4 bg-white shadow-md rounded-lg text-gray-700"
         >
-          <h2 className="text-xl font-semibold">{product.title}</h2>
+          <h2 className="text-xl font-semibold">
+            {" "}
+            <Link href={`/products-db/${product.id}`}>{product.title}</Link>
+          </h2>
           <p>{product.description}</p>
           <p className="text-lg font-medium">${product.price}</p>
         </li>
